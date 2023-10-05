3c1f5f8393ff69ca91710a0c7d595773d57f9fe0
Ahsan Ali <97602040+AhsanAli05@users.noreply.github.com>
2023-10-05 18:07:34 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,88 +1,80 @@
-const quizQuestions = [
-    {
-        question: "What is the capital of France?",
-        options: ["Berlin", "Madrid", "Paris", "Rome"],
-        correctAnswer: 2
-    },
-    {
-        question: "Which planet is known as the 'Red Planet'?",
-        options: ["Venus", "Earth", "Mars", "Jupiter"],
-        correctAnswer: 2
-    },
-    {
-        question: "What is the largest mammal on Earth?",
-        options: ["Elephant", "Giraffe", "Blue Whale", "Hippopotamus"],
-        correctAnswer: 2
-    },
-    {
-        question: "Which gas do plants absorb from the atmosphere?",
-        options: ["Oxygen", "Carbon Dioxide", "Nitrogen", "Hydrogen"],
-        correctAnswer: 1
-    },
-    {
-        question: "Who wrote the play 'Romeo and Juliet'?",
-        options: ["William Shakespeare", "Charles Dickens", "Jane Austen", "Leo Tolstoy"],
-        correctAnswer: 0
+let addbt1 = document.querySelectorAll(".container button")[0];
+let list = document.getElementById("taskList");
+addbt1.addEventListener("click",() => {
+    let input = document.getElementById("taskInput");
+    let listElemnts = document.createElement("li");
+    let listtext = document.createTextNode(input.value);
+    listElemnts.appendChild(listtext);
+    list.appendChild(listElemnts);
+    // for indivdaul list delete 
+    let delbtn = document.createElement("button")
+    let deltext = document.createTextNode("delete");
+    delbtn.appendChild(deltext);
+    listElemnts.appendChild(delbtn);
+    delbtn.setAttribute('onclick', 'deleteTask(this)');
+    // for individual modification
+    let editbtn = document.createElement("button");
+    let edittext = document.createTextNode("edit");
+    editbtn.appendChild(edittext);
+    editbtn.setAttribute('onclick', 'editTask(this)')
+    listElemnts.appendChild(editbtn);
+    input.value = "";
+})
+
+function deleteTask(button) {
+    button.parentNode.remove();
+}
+
+function editTask(button){
+    // to target button parent means li value
+    let currentli = button.parentNode.firstChild.nodeValue;
+    let inputField = prompt("enter update value : ",currentli);
+    button.parentNode.firstChild.nodeValue = inputField;
+}
+
+// for delelte all button
+let deleAll = document.querySelectorAll(".container button")[1];
+deleAll.addEventListener("click",()=>{
+    let lastChild = list.lastChild;
+    while (lastChild) {
+        list.removeChild(lastChild);
+        lastChild = list.lastChild;
     }
-    // Add more questions here
-];
-const question = document.querySelector(".question");
-let options1 = document.querySelector("#option1")
-let options2 = document.querySelector("#option2")
-let options3 = document.querySelector("#option3")
-let options4 = document.querySelector("#option4")
-let submit = document.querySelector("#submit");
+})
 
-let showScore = document.querySelector("#showScore"); 
-let answers = document.querySelectorAll(".answer"); // for getting the all options
-let score = 0; // for count score
+// chatgpt
+// Get references to HTML elements
+// const taskInput = document.getElementById('taskInput');
+// const addTaskButton = document.getElementById('addTask');
+// const taskList = document.getElementById('taskList');
 
-let questionCount = 0 ;// for no of questions
-const loadQustion = () =>{
-    let questionList = quizQuestions[questionCount]; // call  quiQuestion object 
-    question.innerHTML = questionList.question;
-    options1.innerHTML = questionList.options[0];
-    options2.innerHTML = questionList.options[1];
-    options3.innerHTML = questionList.options[2];
-    options4.innerHTML = questionList.options[3];
-}
-loadQustion();
-function selectedOptions() {
-    let answer ;
-    answers.forEach((currentAnw)=>{ // option value is selected answer by user
-        if(currentAnw.checked){
-            answer = currentAnw.id; /// get id of that option 
-        }
-    });
-    return answer; 
-}
-let deSelectAll = ()=>{
-    answers.forEach((optValue)=>{
-        optValue.checked = false
-    });
-}
+// // Add a new task to the list
+// addTaskButton.addEventListener('click', () => {
+//     const taskText = taskInput.value.trim();
+//     if (taskText !== '') {
+//         const listItem = document.createElement('li');
+//         listItem.innerHTML = `
+//             <input type="text" value="${taskText}" readonly>
+//             <button class="edit">Edit</button>
+//             <button class="delete">Delete</button>
+//         `;
 
-
-submit.addEventListener("click",()=>{
-    let selectedOpt = selectedOptions() // id of selected option by user
-    let correctOpt = `ans${quizQuestions[questionCount].correctAnswer + 1}`; // Correct option ID 
-    if(selectedOpt === correctOpt ){
+//         const editButton = listItem.querySelector('.edit');
+//         const deleteButton = listItem.querySelector('.delete');
         
-        score++;
+//         // Add event listeners for edit and delete buttons
+//         editButton.addEventListener('click', () => {
+//             const inputField = listItem.querySelector('input');
+//             inputField.removeAttribute('readonly');
+//         });
+
+//         deleteButton.addEventListener('click', () => {
+//             listItem.remove();
+//         });
+
+//         taskList.appendChild(listItem);
+//         taskInput.value = '';
+//     }
+// });
+
     
-    }
-    questionCount++; // to get new question
-    deSelectAll();
-    if(questionCount < quizQuestions.length ){
-        loadQustion();
-    }
-    else{
-        showScore.innerHTML = `
-        <h3> you score ${score} /${quizQuestions.length} </h3>
-        <button class="btn" onclick = "location.reload()">play again</button> 
-        `;
-        showScore.classList.remove("scoreArea");
-    }
-    
-    
-});
